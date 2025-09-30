741b3e91d1c5cbd77a3ef06624cf845f8bd7f05c
ks_warrior <kswarriorpro@gmail.com>
2025-09-30 11:34:40 +0530

    Update root-install.sh

--- root-install.sh
+++ root-install.sh
@@ -10,10 +10,18 @@ fi
 mkdir -p /usr/local/bin
 if curl -fsSL "https://github.com/KSWarrior/ksnova/releases/latest/download/ksnova-linux" -o "/usr/local/bin/ksn"; then
     chmod +x "/usr/local/bin/ksn"
-    echo "✅ KS Nova installed successfully!"
+    echo "✅ KS Nova download successfully!"
     echo "👤 Author: KS Warrior"
     echo "🌐 More info: https://github.com/KSWarrior/ksnova"
     echo "🚀 You can now run 'ksn' from anywhere."
+else
+    echo "❌ Failed to download KS Nova."
+    echo "🔗 Visit the official site for manual installation: https://github.com/KSWarrior/ksnova"
+    exit 1
+fi
+
+if command -v ksn >/dev/null 2>&1; then
+    echo "✅ KS Nova installation successful!"
 else
     echo "❌ Failed to download KS Nova."
     echo "🔗 Visit the official site for manual installation: https://github.com/KSWarrior/ksnova"
